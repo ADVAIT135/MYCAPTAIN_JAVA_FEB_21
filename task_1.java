9c2d8fc921ebed54cb29dcd1533951637f6cae70
ADVAIT GURUNATH CHAVAN <59262727+ADVAIT135@users.noreply.github.com>
2021-02-08 01:17:53 +0530

    My_Captain_JAVA_task_1
    This code accepts name from the user and then displays a hello message for the user along with his/her name

---
+++ task_1.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package My_Captain_Task_1;
+
+/**
+ * @author Advait
+ *
+ */
+import java.util.*;
+public class task_1 {
+	public static void main(String[] args) {
+		System.out.println("Enter Your Name :-");
+		Scanner name = new Scanner(System.in);
+		String input = name.next();		
+		name.close();
+		System.out.println("Hi There "+input+" !!!!!!!");
+	}
+
+}
